70d0a9e2c38e62146fd4601a9faaf1d1f5a225a0
quincy <insta0430@gmail.com>
2023-06-07 20:48:20 +0900

    update

---
+++ NSGA II.py
@@ -0,0 +1,79 @@
+import pygad
+import numpy
+import random
+import drawByCV
+
+NUMBER_OF_CIRCLE = drawByCV.NUMBER_OF_CIRCLE
+
+# function_inputs = []
+# for i in range(NUMBER_OF_CIRCLE*2):
+#     function_inputs.append(800*random.random())
+
+def fitness_func(ga_instance, solution, solution_idx):
+    # Calculating the fitness value of each solution in the current population.
+    # The fitness function calulates the sum of products between each input and its corresponding weight.
+    return drawByCV.getFitness(solution)
+
+fitness_function = fitness_func
+
+num_generations = 2 # Number of generations.
+num_parents_mating = 7 # Number of solutions to be selected as parents in the mating pool.
+
+init_range_low = 0
+init_range_high = 800
+
+# To prepare the initial population, there are 2 ways:
+# 1) Prepare it yourself and pass it to the initial_population parameter. This way is useful when the user wants to start the genetic algorithm with a custom initial population.
+# 2) Assign valid integer values to the sol_per_pop and num_genes parameters. If the initial_population parameter exists, then the sol_per_pop and num_genes parameters are useless.
+sol_per_pop = 30 # Number of solutions in the population.
+num_genes = NUMBER_OF_CIRCLE*2  # (x, y) * 50
+gene_type = int
+
+last_fitness = 0
+def callback_generation(ga_instance):
+    global last_fitness
+    best_solution = ga_instance.best_solution()
+    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+    print("Fitness    = {fitness}".format(fitness=best_solution[1]))
+    print("Change     = {change}".format(change=best_solution[1] - last_fitness))
+    print(ga_instance.best_solution()[0])
+    print("\n\n")
+    last_fitness = best_solution[1]
+
+# Creating an instance of the GA class inside the ga module. Some parameters are initialized within the constructor.
+ga_instance = pygad.GA(num_generations=num_generations,
+                       num_parents_mating=num_parents_mating, 
+                       fitness_func=fitness_function,
+                       sol_per_pop=sol_per_pop, 
+                       num_genes=num_genes,
+                       gene_type=gene_type,
+
+                       init_range_low=init_range_low,
+                       init_range_high=init_range_high,
+
+                       on_generation=callback_generation)
+
+# Running the GA to optimize the parameters of the function.
+ga_instance.run()
+
+# After the generations complete, some plots are showed that summarize the how the outputs/fitenss values evolve over generations.
+ga_instance.plot_fitness()
+
+# Returning the details of the best solution.
+solution, solution_fitness, solution_idx = ga_instance.best_solution()
+print("Parameters of the best solution : {solution}".format(solution=solution))
+print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+print("Index of the best solution : {solution_idx}".format(solution_idx=solution_idx))
+
+drawByCV.visualize(solution)
+
+if ga_instance.best_solution_generation != -1:
+    print("Best fitness value reached after {best_solution_generation} generations.".format(best_solution_generation=ga_instance.best_solution_generation))
+
+# Saving the GA instance.
+filename = 'genetic' # The filename to which the instance is saved. The name is without extension.
+ga_instance.save(filename=filename)
+
+# Loading the saved GA instance.
+loaded_ga_instance = pygad.load(filename=filename)
+loaded_ga_instance.plot_fitness()
---
+++ makeImgae.py
@@ -0,0 +1,64 @@
+import random
+from PIL import Image, ImageDraw
+
+def generate_map(room_width, room_height, num_structures, padding):
+    # 큰 직사각형의 크기 설정
+    rect_width = random.randint(300, 700)
+    rect_height = random.randint(300, 700)
+    rect_x = 100
+    rect_y = 100
+
+    # 큰 직사각형 생성
+    room_image = Image.new('RGB', (room_width, room_height), color='white')
+    room_draw = ImageDraw.Draw(room_image)
+
+    # 큰 직사각형의 굵은 선 그리기
+    room_draw.rectangle([(rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height)], outline='black', width=8)
+
+    structures = []
+    for _ in range(num_structures):
+        structure_type = random.choice(['rectangle', 'circle'])  # 구조물 유형 선택
+
+        if structure_type == 'rectangle':
+            structure_width = random.randint(40, 50)  # 구조물 너비 (랜덤)
+            structure_height = random.randint(40, 50)  # 구조물 높이 (랜덤)
+
+            structure_x = random.randint(rect_x + padding, rect_x + rect_width - padding - structure_width)  # 구조물 x 좌표 (랜덤)
+            structure_y = random.randint(rect_y + padding, rect_y + rect_height - padding - structure_height)  # 구조물 y 좌표 (랜덤)
+
+            room_draw.rectangle((structure_x, structure_y, structure_x + structure_width, structure_y + structure_height),
+                                fill='black')
+            structures.append((structure_type, structure_x, structure_y, structure_width, structure_height))
+        elif structure_type == 'circle':
+            structure_radius = random.randint(5, 25)  # 구조물 반지름 (랜덤)
+
+            # 원의 중심 좌표를 구하기 위해 구조물 반지름과 패딩을 고려하여 좌표 범위를 설정
+            min_x = rect_x + padding + structure_radius
+            max_x = rect_x + rect_width - padding - structure_radius
+            min_y = rect_y + padding + structure_radius
+            max_y = rect_y + rect_height - padding - structure_radius
+
+            structure_center_x = random.randint(min_x, max_x)  # 구조물 중심 x 좌표 (랜덤)
+            structure_center_y = random.randint(min_y, max_y)  # 구조물 중심 y 좌표 (랜덤)
+
+            room_draw.ellipse((structure_center_x - structure_radius, structure_center_y - structure_radius,
+                               structure_center_x + structure_radius, structure_center_y + structure_radius),
+                              fill='black')
+            structures.append((structure_type, structure_center_x, structure_center_y, structure_radius))
+
+    return room_image, structures
+
+def save_image(image, filename):
+    image.save(filename, 'PNG')
+
+# 방 크기, 구조물 개수, 벽과의 간격 설정
+room_width = 800
+room_height = 800
+num_structures = 15
+padding = 10
+
+# 방 생성 및 구조물 배치
+room_image, structures = generate_map(room_width, room_height, num_structures, padding)
+
+# 이미지 저장
+save_image(room_image, 'room_map.png')
